bc291f37b2b5ee9e8efe7244935426297448d827
harsh singh <harshsingh@harshs-MacBook-Air.local>
2016-01-26 18:15:34 +0530

    make it better

---
+++ index.js
@@ -0,0 +1,53 @@
+var express=require('express');
+var app=express();
+var mongojs = require('mongojs');
+var db = mongojs('warehouse',['warehouse']);
+var bodyParser = require('body-parser'); 
+
+app.use(express.static(__dirname + '/static'));
+app.use(bodyParser.json());
+
+app.get('/warehouse',function(req,res) {
+        console.log('I received a get request');
+        db.warehouse.find(function(err,docs){
+            console.log(docs);
+            res.json(docs);
+        })
+});
+
+app.post('/warehouse', function(req,res){
+        console.log(req.body);
+        db.warehouse.insert(req.body, function(err,docs){
+            res.json(docs);
+        });
+});
+
+app.delete('/warehouse/:id', function(req,res){
+    var id = req.params.id;
+    console.log(id);
+    db.warehouse.remove({_id: mongojs.ObjectId(id)}, function(err,doc){
+        res.json(doc);
+    });
+});
+
+app.get('/warehouse/:id', function(req,res){
+    var id = req.params.id;
+    console.log(id);
+    db.warehouse.findOne({_id: mongojs.ObjectId(id)}, function(err,doc){
+            res.json(doc);
+    });
+});
+
+app.put('/warehouse/:id', function(req,res){
+    var id = req.params.id;
+    console.log(req.body.name);
+    db.warehouse.findAndModify({
+        query : {_id : mongojs.ObjectId(id)},
+        update : {$set : {description:req.body.description, price:req.body.price, stock:req.body.stock, packing:req.body.packing }},
+        new : true }, function(err,doc){
+                res.json(doc);
+        });
+});
+
+app.listen(3000);
+console.log("server running on localhost:3000");
---
+++ static/app/app.js
@@ -0,0 +1,177 @@
+var app = angular.module('Inventoryapp',['ngRoute','ngAnimate','ui.bootstrap']);
+
+app.config(['$routeProvider',function($routeProvider) {
+    $routeProvider.
+        when('/',{
+            templateUrl:'partials/main.html',
+            controller:'MainCtrl'
+        }).
+        when('/home',{
+            templateUrl:'partials/product.html',
+            controller:'ProductCtrl'
+        }).
+        otherwise({
+            redirectTo:'/'
+        });
+}]);
+
+
+
+app.controller('MainCtrl', function($scope,$location){
+    $scope.go=function(){
+        $location.path('/home');
+    };
+});
+
+app.controller('ProductCtrl', function($scope,$location,$modal,$filter,$http){
+
+    $scope.product={};
+
+    console.log("Hello World from controller");
+
+    var refresh = function(){
+        $http.get('/warehouse').success(function(response){
+            console.log('I got the requested data');
+            $scope.warehouse=response;
+            $scope.products={};
+        });
+    };  
+
+    refresh();
+
+    $scope.cols=[
+                    {text:'ID',predicate:'id',sortable:true,dataType:"number"},
+                    {text:'Name',predicate:'name',sortable:true},
+                    {text:'Price',predicate:'price',sortable:true},
+                    {text:'Stock',predicate:'stock',sortable:true},
+                    {text:'Packing',predicate:'packing',sortable:true,dataType:"number"},
+                    {text:'Description',predicate:'description',sortable:true},
+                    {text:'Status',predicate:'status',sortable:true},
+                    {text:'Action',predicate:"",sortable:true}                
+    ];
+
+    $scope.open=function(p,size){
+        var modalInstance = $modal.open({
+            templateUrl:'partials/edit.html',
+            controller:'EditCtrl',
+            size:size,
+            resolve: {
+                item: function(){
+                    return p;
+                }
+            }
+        });
+         
+    }
+
+    $scope.deleteProduct=function(id){
+        if(confirm("Are you sure to remove the product")){
+        $http.delete('/warehouse/' + id).success(function(response){
+            refresh();
+                });
+        }
+    };
+    
+});
+
+app.controller('EditCtrl', function($scope,$modalInstance,item,$http){
+    
+
+    $scope.product=angular.copy(item);
+
+    
+    $scope.cancel=function(){
+        $modalInstance.dismiss('Close');
+    };
+   
+    $scope.title = (item.id>0) ? "Add product" : "Edit product"; 
+    $scope.buttonText = (item.id>0) ? "Update product" : "Add new product";
+    
+        var original = item;
+        $scope.isClean = function() {
+            return angular.equals(original, $scope.product);
+        }
+
+
+    $scope.AddProduct=function(product,id){
+            if (product.id>0 ) {
+                $http.put('/warehouse/' + id,product).success(function(response){
+                        if(response.status != 'error'){
+                            var x = angular.copy(product);
+                            x.save='update';
+                            $modalInstance.close(x);
+                        }else{ 
+                            console.log(response);
+                        }
+                });
+            }else{
+                product.status = 'In Stock';    
+                $http.post('/warehouse',product).success(function(response){
+                       if(response.status != 'error'){
+                            var x = angular.copy(product);
+                            x.save = 'insert';
+                            $modalInstance.close(x);
+                       }else{
+                        console.log(response);
+                       } 
+               });
+            } 
+    };
+
+
+});
+
+app.directive('formelement',function(){
+    return{
+        restrict:"E",
+        transclude:true,
+        scope:{
+            label:"@",
+            model:"="
+        },
+        link:function(scope,element,attrs){
+                scope.disabled = attrs.hasOwnProperty('disabled');
+                scope.requied =  attrs.hasOwnProperty('required');
+                scope.pattern =  attrs.pattern || '.*'; 
+        },
+            template: '<div class="form-group"><label class="col-sm-3 control-label no-padding-right" > {{label}} </label><div class="col-sm-7"><span class="block input-icon input-icon-right" ng-transclude></span></div></div>'
+    
+    };
+});
+
+app.directive('onlynum',function(){
+    return function(scope,element,attrs){
+        
+                var keyCode = [8,9,13,37,39,46,48,49,50,51,52,53,54,55,56,57,96,97,98,99,100,101,102,103,104,105,110,190];
+
+                element.bind('keydown', function(event){
+                        if($.inArray(event.which,keyCode)== -1){
+                            scope.$apply(function(){
+                            scope.$eval(attrs.onlyNum);
+                            event.preventDefault();
+                            });
+                            event.preventDefault();
+                        }
+                });
+    };
+});
+
+app.directive('focus', function() {
+    return function(scope, element) {
+        element[0].focus();
+    }      
+});
+
+app.directive('animateOnChange',function($animate){
+    return function(scope,element,attrs){
+        scope.$watch(attrs.animateOnChange, function(nv,ov) {
+            if (nv!=ov) {
+                    var c = 'change-up';
+                      $animate.addClass(elem,c, function() {
+                      $animate.removeClass(elem,c);
+                            });
+             }
+      });  
+    };
+});
+
